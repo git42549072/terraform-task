521c40063b50c6de868783b5a0a02682ccb17426
git42549072 <36084794+git42549072@users.noreply.github.com>
2020-10-27 01:36:53 +0530

    ec2-launch-code

---
+++ instance.tf
@@ -0,0 +1,41 @@
+resource "aws_instance" "instance-1" {
+  ami           = var.AMIS[var.AWS_REGION]
+  instance_type = "t2.micro"
+
+  # the VPC subnet
+  subnet_id = aws_subnet.main-public-1.id
+
+  # the security group
+  vpc_security_group_ids = [aws_security_group.allow-ssh.id]
+
+  # the public SSH key
+  key_name = aws_key_pair.mykeypair.key_name
+}
+
+resource "aws_instance" "instance-2" {
+  ami           = var.AMIS[var.AWS_REGION]
+  instance_type = "t2.micro"
+
+  # the VPC subnet
+  subnet_id = aws_subnet.main-public-2.id
+
+    # the security group
+  vpc_security_group_ids = [aws_security_group.allow-ssh.id]
+
+  # the public SSH key
+  key_name = aws_key_pair.mykeypair.key_name
+}
+
+resource "aws_instance" "instance-3" {
+  ami           = var.AMIS[var.AWS_REGION]
+  instance_type = "t2.micro"
+
+  # the VPC subnet
+  subnet_id = aws_subnet.main-public-3.id
+
+    # the security group
+  vpc_security_group_ids = [aws_security_group.allow-ssh.id]
+
+  # the public SSH key
+  key_name = aws_key_pair.mykeypair.key_name
+}
